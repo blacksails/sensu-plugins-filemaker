78c8810ae5ca21cc88469a4c424d70d1cda0922e
Benjamin Nørgaard <b@noergaard.dk>
2017-02-23 16:50:42 +0100

    Fix bugs

--- bin/check-filemaker.rb
+++ bin/check-filemaker.rb
@@ -5,13 +5,13 @@ require 'sensu-plugins-filemaker'
 
 class CheckFilemaker < Sensu::Plugin::Check::CLI
   def run
-    unless SensuPluginsFilemaker.Server.server_running?
+    unless SensuPluginsFilemaker::Server.server_running?
       critical "Filemaker server is not running"
     end
-    unless SensuPluginsFilemaker.Server.cwp_running?
+    unless SensuPluginsFilemaker::Server.cwp_running?
       critical "Filemaker CWP is not running"
     end
-    unless SensuPluginsFilemaker.Server.admin_server_running?
+    unless SensuPluginsFilemaker::Server.admin_server_running?
       warning "Filemaker admin server is not running"
     end
     ok "Filemaker and it's services are running"
--- lib/sensu-plugins-filemaker/server.rb
+++ lib/sensu-plugins-filemaker/server.rb
@@ -1,18 +1,18 @@
 module SensuPluginsFilemaker
   class Server
     def self.server_running?
-      %(ps aux | grep [f]mserverd)
-      $?.exitcode == 0
+      %x(ps aux | grep [f]mserverd)
+      $?.exitstatus == 0
     end
 
     def self.cwp_running?
-      %(ps aux | grep [f]mscwpc)
-      $?.exitcode == 0
+      %x(ps aux | grep [f]mscwpc)
+      $?.exitstatus == 0
     end
 
     def self.admin_server_running?
-      %(ps aux | grep [f]msased)
-      $?.exitcode == 0
+      %x(ps aux | grep [f]msased)
+      $?.exitstatus == 0
     end
   end
 end
